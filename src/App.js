44744c61fbb34c62419ab7d39455e0ef4838a325
kasima <yama10705988.dev@gmail.com>
2019-01-02 21:09:52 +0900

    ９章ルータの基本

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import MyCheckMulti from './MyCheckMulti';
+import MySelect from './MySelect';
+import MyTextarea from './MyTextarea';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">MyCheckMulti</Link></li>
+            <li><Link to="/select">MySelect</Link></li>
+            <li><Link to="/textarea">MyTextarea</Link></li>
+          </ul>
+          <hr/>
+          <Switch>
+            <Route exact path="/" component={MyCheckMulti}/>
+            <Route path="/select" component={MySelect}/>
+            <Route path="/textarea" component={MyTextarea}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
-
-export default App;
