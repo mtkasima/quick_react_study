af726dfb1b811895f061e03b93212d65a7a6b1cb
kasima <yama10705988.dev@gmail.com>
2019-01-02 21:19:12 +0900

    ９章ルートパラメタ

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import MyCheckMulti from './MyCheckMulti';
-import MySelect from './MySelect';
-import MyTextarea from './MyTextarea';
+import MyArticle from './MyArticle';
 
 export default class App extends Component {
   render() {
@@ -10,15 +8,12 @@ export default class App extends Component {
       <Router>
         <div>
           <ul>
-            <li><Link to="/">MyCheckMulti</Link></li>
-            <li><Link to="/select">MySelect</Link></li>
-            <li><Link to="/textarea">MyTextarea</Link></li>
+            <li><Link to="/article/1">記事No.1</Link></li>
+            <li><Link to="/article/10">記事No.10</Link></li>
           </ul>
           <hr/>
           <Switch>
-            <Route exact path="/" component={MyCheckMulti}/>
-            <Route path="/select" component={MySelect}/>
-            <Route path="/textarea" component={MyTextarea}/>
+            <Route exact path="/article/:id" component={MyArticle}/>
           </Switch>
         </div>
       </Router>
---
+++ src/MyArticle.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class MyArticle extends Component {
+  render(){
+    const id = this.props.match.params.id;
+    return(
+      <div>No.{id}の記事情報</div>
+    );
+  }
+}
